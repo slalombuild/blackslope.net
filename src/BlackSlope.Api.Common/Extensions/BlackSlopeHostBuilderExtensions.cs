31d40191e8e0679d41e4ada29ea9b4dbfeba98c8
Parham <Parham_motameni@live.com>
2020-02-22 15:02:06 -0600

    Updated packages to latest versions, and some clean ups

--- src/BlackSlope.Api.Common/Extensions/BlackSlopeHostBuilderExtensions.cs
+++ src/BlackSlope.Api.Common/Extensions/BlackSlopeHostBuilderExtensions.cs
@@ -12,24 +12,27 @@ namespace BlackSlope.Api.Common.Extensions
     public static class BlackSlopeHostBuilderExtensions
     {
         public static IWebHostBuilder UseSerilog(this IWebHostBuilder webHostBuilder, string appSettingsSection) =>
-             webHostBuilder.UseSerilog((ctx, config) =>
-                {
-                    var appSettings = ctx.Configuration.GetSection(appSettingsSection).Get<HostConfig>();
-                    var serilogConfig = appSettings.Serilog;
+            webHostBuilder.UseSerilog((ctx, config) =>
+            {
+                var appSettings = ctx.Configuration.GetSection(appSettingsSection).Get<HostConfig>();
+                var serilogConfig = appSettings.Serilog;
 
-                    // If they follow conventions for Serilog in config it will be read here.
-                    config.ReadFrom.Configuration(ctx.Configuration);
+                // If they follow conventions for Serilog in config it will be read here.
+                config.ReadFrom.Configuration(ctx.Configuration);
 
-                    SetLogLevel(config, serilogConfig);
-                    LogToFile(config, serilogConfig);
-                    LogToConsole(config, serilogConfig);
-                    LogToApplicationInsights(config, appSettings, serilogConfig);
-                });
+                SetLogLevel(config, serilogConfig);
+                LogToFile(config, serilogConfig);
+                LogToConsole(config, serilogConfig);
+                LogToApplicationInsights(config, appSettings, serilogConfig);
+            });
 
         private static void LogToApplicationInsights(LoggerConfiguration config, HostConfig appSettings, SerilogConfig serilogConfig)
         {
             if (serilogConfig.WriteToAppInsights)
             {
+#pragma warning disable CS0618  // Suppress warning since we want to be able to log early error,
+                // remove when the issue (https://github.com/serilog/serilog-sinks-applicationinsights/issues/121) is closed
+
                 // TODO: TelemetryConverter should be configurable (defaulted to Trace)
                 // Note: best practice is to use the existing Telemetry
                 if (string.IsNullOrEmpty(TelemetryConfiguration.Active.InstrumentationKey))
@@ -42,6 +45,7 @@ namespace BlackSlope.Api.Common.Extensions
                     config.WriteTo.ApplicationInsights(TelemetryConfiguration.Active, TelemetryConverter.Traces);
                     config.WriteTo.ApplicationInsights(TelemetryConfiguration.Active, TelemetryConverter.Events);
                 }
+#pragma warning restore CS0618 // Type or member is obsolete
             }
         }
 
